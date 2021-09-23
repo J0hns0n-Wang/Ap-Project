bd57f7a8e795e5c147eace2935186f56ebe417ab
J0hns0n-Wang <johnsonwang529@gmail.com>
2021-09-23 12:30:33 -0400

    df

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 console.log("connected")
 
 const balance = document.querySelector(".balance")
-const incomeAmount = document.querySelector(".income-header")
+const totalIncome = document.querySelector(".income-header")
+const incomeHistory = document.querySelector(".income-history")
+const totalExpense = document.querySelector(".expense-header")
+const expenseHistory = document.querySelector(".expense-history")
+const delBtn = document.querySelector(".del-button")
+const addBtn = document.querySelector(".add-button")
+const addForm = document.querySelector(".add-form")
+const addArea = document.querySelector(".add-form-area")
+
+console.log(addArea)
+
+let incomeHistoryarr = []
+let expenseHistoryarr = []
+let balanceNumber = 0
+let totalIncomeNumber = 0
+let totalExpenseNumber = 0
+
+balance.innerHTML = `Balance: $${balanceNumber}`
+
+
