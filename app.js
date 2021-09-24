ba5be3f85d9e081d9f3cf6905a9f84561021188c
J0hns0n-Wang <johnsonwang529@gmail.com>
2021-09-24 12:27:47 -0400

    ynhgfnty

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const delBtn = document.querySelector(".del-button")
 const addBtn = document.querySelector(".add-button")
 const addForm = document.querySelector(".add-form")
 const addArea = document.querySelector(".add-form-area")
+const addName = document.querySelector(".add-name-area")
 
-console.log(addArea)
+console.log(addName)
 
 let incomeHistoryarr = []
 let expenseHistoryarr = []
@@ -19,5 +20,53 @@ let totalIncomeNumber = 0
 let totalExpenseNumber = 0
 
 balance.innerHTML = `Balance: $${balanceNumber}`
+totalIncome.innerHTML = `Balance: $${totalIncomeNumber}`
+totalExpense.innerHTML = `Balance: $${totalExpenseNumber}`
+
+let date = new Date()
+let month = date.getMonth() + 1 //.getMonth returns number from 0 to 11
+let day = date.getDate()
+let year = date.getFullYear()
+let dateNow = `${month}/${day}/${year}`
+console.log(dateNow)
 
 
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const addNameParam = addName.value
+    const addValueParam = addArea.value
+    let income = {
+        date: `${dateNow}`,
+        name: `${addNameParam}`,
+        amount:`${addValueParam}`
+        
+    }
+    let expense = {
+        date: `${dateNow}`,
+        name: `${addNameParam}`,
+        amount:`${addValueParam}`
+        
+    }
+    if(addValueParam !=  Number){
+        alert("hi")
+    }
+    if(addValueParam < 0){
+        expenseHistoryarr.push(expense)
+    }else{
+        incomeHistoryarr.push(income)
+    }
+    // switch('hi'){
+    //     case addValueParam != Number:
+    //         alert('hi');
+    //         break;
+    //         case addValueParam < 0:
+    //             expenseHistoryarr.push(expense);
+    //             break;
+    //             default:
+    //                 incomeHistoryarr.push(income)
+    // }
+console.log(addNameParam)
+console.log(addValueParam)
+console.log(incomeHistoryarr)
+console.log(expenseHistoryarr)
+})
