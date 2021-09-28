021aef15a05fdc7a912b9345f8289f27a7f1dcc6
J0hns0n-Wang <johnson.wang23@sitechhs.com>
2021-09-27 20:12:24 -0400

    ytju

--- app.js
+++ app.js
@@ -1,75 +1,68 @@
-console.log("connected")
+console.log("connected");
 
-const balance = document.querySelector(".balance")
-const totalIncome = document.querySelector(".income-header")
-const incomeHistory = document.querySelector(".income-history")
-const totalExpense = document.querySelector(".expense-header")
-const expenseHistory = document.querySelector(".expense-history")
-const delBtn = document.querySelector(".del-button")
-const addBtn = document.querySelector(".add-button")
-const addForm = document.querySelector(".add-form")
-const addArea = document.querySelector(".add-form-area")
-const addName = document.querySelector(".add-name-area")
+const balance = document.querySelector(".balance");
+const totalIncome = document.querySelector(".income-header");
+const incomeHistory = document.querySelector(".income-history");
+const totalExpense = document.querySelector(".expense-header");
+const expenseHistory = document.querySelector(".expense-history");
+const delBtn = document.querySelector(".del-button");
+const addBtn = document.querySelector(".add-button");
+const addForm = document.querySelector(".add-form");
+const addArea = document.querySelector(".add-form-area");
+const addName = document.querySelector(".add-name-area");
 
-console.log(addName)
+console.log(addName);
 
-let incomeHistoryArr = []
-let expenseHistoryArr = []
-let balanceNumber = 0
-let totalIncomeNumber = 0
-let totalExpenseNumber = 0
+let incomeHistoryArr = [];
+let expenseHistoryArr = [];
+let balanceNumber = 0;
+let totalIncomeNumber = 0;
+let totalExpenseNumber = 0;
 
-balance.innerHTML = `Balance: $${balanceNumber}`
-totalIncome.innerHTML = `Balance: $${totalIncomeNumber}`
-totalExpense.innerHTML = `Balance: $${totalExpenseNumber}`
+balance.innerHTML = `Balance: $${balanceNumber}`;
+totalIncome.innerHTML = `Balance: $${totalIncomeNumber}`;
+totalExpense.innerHTML = `Balance: $${totalExpenseNumber}`;
 
-let date = new Date()
-let month = date.getMonth() + 1 //.getMonth returns number from 0 to 11
-let day = date.getDate()
-let year = date.getFullYear()
-let dateNow = `${month}/${day}/${year}`
-console.log(dateNow)
+let date = new Date();
+let month = date.getMonth() + 1; //.getMonth returns number from 0 to 11
+let day = date.getDate();
+let year = date.getFullYear();
+let dateNow = `${month}/${day}/${year}`;
+console.log(dateNow);
 
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let incomeHistoryArr = [];
+  const addNameParam = addName.value;
+  const addValueParam = addArea.value;
+  let income = {
+    date: `${dateNow}`,
+    name: `${addNameParam}`,
+    amount: `${addValueParam}`,
+  };
+  let expense = {
+    date: `${dateNow}`,
+    name: `${addNameParam}`,
+    amount: `${addValueParam}`,
+  };
+  // if(addValueParam !=  Number){
+  //     alert("hi")
+  // }
+  if (addValueParam < 0) {
+    expenseHistoryArr.push(expense);
+  }
+  if (addValueParam > 0) {
+    incomeHistoryArr.push(income);
+  }
+  // if (addValueParam != Number){
+  //   break;
+  // }
 
-addBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const addNameParam = addName.value
-    const addValueParam = addArea.value
-    let income = {
-        date: `${dateNow}`,
-        name: `${addNameParam}`,
-        amount:`${addValueParam}`
-        
-    }
-    let expense = {
-        date: `${dateNow}`,
-        name: `${addNameParam}`,
-        amount:`${addValueParam}`
-        
-    }
-    // if(addValueParam !=  Number){
-    //     alert("hi")
-    // }
-    if(addValueParam < 0){
-        expenseHistoryArr.push(expense)
-    }else{
-        incomeHistoryArr.push(income)
-    }
-    // switch('hi'){
-    //     case addValueParam != Number:
-    //         alert('hi');
-    //         break;
-    //         case addValueParam < 0:
-    //             expenseHistoryarr.push(expense);
-    //             break;
-    //             default:
-    //                 incomeHistoryarr.push(income)
-    // }
-console.log(addNameParam)
-console.log(addValueParam)
-console.log(incomeHistoryArr)
-console.log(expenseHistoryArr)
-incomeHistoryArr.forEach((income) => {
+  console.log(addNameParam);
+  console.log(addValueParam);
+  console.log(incomeHistoryArr);
+  console.log(expenseHistoryArr);
+  incomeHistoryArr.forEach((income) => {
     incomeHistory.insertAdjacentHTML(
       "beforeend",
       `<div class="income-date">${income.date}</div>
@@ -87,11 +80,8 @@ incomeHistoryArr.forEach((income) => {
       <button type="button" class="del-button">Delete</button> `
     );
   });
- 
-  let newIncome = addValueParam
-  totalIncome.innerHTML = `$${newIncome}`
-  let newBalance = balanceNumber + newIncome
-  balance.innerHTML = `$${newBalance}`
 
-})
-
+  totalIncome.innerHTML = `$${newIncome}`;
+  let newBalance = balanceNumber + newIncome;
+  balance.innerHTML = `$${newBalance}`;
+});
